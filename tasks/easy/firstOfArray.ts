b3b83dc67b0d970ed97d4b5c0ea905530b430d9e
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-07-01 03:31:23 +0900

    add firstOfArray

---
+++ tasks/easy/firstOfArray.ts
@@ -0,0 +1,17 @@
+// type arr1 = ['a', 'b', 'c']
+// type arr2 = [3, 2, 1]
+
+// type head1 = First<arr1> // expected to be 'a'
+// type head2 = First<arr2> // expected to be 3
+
+{
+    type arr1 = ['a', 'b', 'c'];
+    type arr2 = [3, 2, 1];
+    type arr3 = [];
+
+    type First<T extends any[]> = T extends [] ? never : T[0];
+
+    type head1 = First<arr1>;
+
+    const tmp: head1 = 'a';
+}
--- tasks/easy/toupleToObject.ts
+++ tasks/easy/toupleToObject.ts
@@ -2,19 +2,21 @@
 
 // type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
 
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+{
+    const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
 
-type TupleToObject<T extends readonly any[]> = {
-    [K in T[number]]: K
-};
+    type TupleToObject<T extends readonly any[]> = {
+        [K in T[number]]: K
+    };
 
-type result = TupleToObject<typeof tuple>;
+    type result = TupleToObject<typeof tuple>;
 
-const tmp: result = {
-    tesla: 'tesla',
-    "model 3": "model 3",
-    "model X": "model X",
-    "model Y": "model Y"
-};
+    const tmp: result = {
+        tesla: 'tesla',
+        "model 3": "model 3",
+        "model X": "model X",
+        "model Y": "model Y"
+    };
 
+}
 
