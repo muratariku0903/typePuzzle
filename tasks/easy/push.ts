6d98270cbc9498d3cb6daabbbeccafc185ef6e8c
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-07-05 22:38:11 +0900

    add push

---
+++ tasks/easy/push.ts
@@ -0,0 +1,14 @@
+// Implement the generic version of Array.push
+
+// For example
+
+// type Result = Push<[1, 2], '3'> // [1, 2, '3']
+
+import { Expect, Equal } from '../../utils';
+
+{
+    type Push<T extends any[], U> = [...T, U];
+    type Unshift<T extends any[], U> = [U, ...T]
+
+    // const nums: Push<[1, 2], 3> = [1, 2, 3]
+}
