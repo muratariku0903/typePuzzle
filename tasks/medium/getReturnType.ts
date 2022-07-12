178f45e60c675248bb8c99892ae777fa47168976
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-07-12 22:55:35 +0900

    add returnType.ts

---
+++ tasks/medium/getReturnType.ts
@@ -0,0 +1,39 @@
+// Implement the built-in ReturnType<T> generic without using it.
+
+// For example
+
+// const fn = (v: boolean) => {
+//   if (v)
+//     return 1
+//   else
+//     return 2
+// }
+
+// type a = MyReturnType<typeof fn> // should be "1 | 2"
+
+import { Expect, Equal } from '../../utils';
+
+{
+    type MyReturnType<T extends Function> = T extends (...Args: any[]) => infer R ? R : never;
+
+    type ComplexObject = {
+        a: [12, 'foo']
+        bar: 'hello'
+        prev(): number
+    }
+
+    const fn = (v: boolean) => v ? 1 : 2
+    const fn1 = (v: boolean, w: any) => v ? 1 : 2
+
+    const tmp: MyReturnType<typeof fn1> = (n: number) => n * 2;
+
+    type cases = [
+        Expect<Equal<string, MyReturnType<() => string>>>,
+        Expect<Equal<123, MyReturnType<() => 123>>>,
+        Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+        Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+        Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+        Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+        Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
+    ];
+}
