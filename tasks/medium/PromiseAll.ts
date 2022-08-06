3733088107da400f396527889cafd959612c2327
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-08-07 03:49:08 +0900

    add PromiseAll

---
+++ tasks/medium/PromiseAll.ts
@@ -0,0 +1,20 @@
+// Type the function PromiseAll that accepts an array of PromiseLike objects, the returning value should be Promise<T> where T is the resolved result array.
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise<string>((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+// // expected to be `Promise<[number, 42, string]>`
+// const p = PromiseAll([promise1, promise2, promise3] as const)
+
+// 実行したらエラーになる
+
+declare function PromiseAll<T extends unknown[]>(values: readonly [...T]): Promise<{
+    [K in keyof T]: T[K] extends Promise<infer PV> ? PV : T[K];
+}>
+
+const p = PromiseAll([promise1, promise2, promise3]);
+
+console.log(p);
