921652e67cf0f453d6f3b2db59cb135bc9046e33
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-03 18:59:37 +0900

    wip

---
+++ tasks/medium/IsUnion.ts
@@ -0,0 +1,25 @@
+// Implement a type IsUnion, which takes an input type T and returns whether T resolves to a union type.
+
+import { Expect, Equal } from '../../utils';
+
+{
+  
+  type IsUnionImpl<T, C extends T = T> = (T extends T ? C extends T ? true : unknown : never) extends true ? false : true;
+  type IsUnion<T> = IsUnionImpl<T>;
+
+  type cases = [
+    Expect<Equal<IsUnion<string>, false>>,
+    Expect<Equal<IsUnion<string | number>, true>>,
+    Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+    Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+    Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+    Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+    Expect<Equal<IsUnion<[string | number]>, false>>,
+    // Cases where T resolves to a non-union type.
+    Expect<Equal<IsUnion<string | never>, false>>,
+    Expect<Equal<IsUnion<string | unknown>, false>>,
+    Expect<Equal<IsUnion<string | any>, false>>,
+    Expect<Equal<IsUnion<string | 'a'>, false>>,
+    Expect<Equal<IsUnion<never>, false>>,
+  ]
+}
