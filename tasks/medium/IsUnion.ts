bb3e2a48ec2dff9898a4ad82ba277f98d06d835c
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-05 19:56:37 +0900

    wip

--- tasks/medium/IsUnion.ts
+++ tasks/medium/IsUnion.ts
@@ -45,44 +45,29 @@ import { Expect, Equal } from '../../utils';
   // type phase7 = (true | unknown) extends true ? false : true;
   // type phase8 = true;
 
-  type case2 = IsUnion<[string | number]>
-  type phase1 = IsUnionImpl<[string | number], [string | number]>;
-  type phase2 =
-    (
-      string | number extends string | number ? string | number extends string | number ? true : unknown : never
-    ) extends true ? false : true;
-  type phase3 =
-    (
-      (string extends string | number ? string | number extends string ? true : unknown : never)
-      |
-      (number extends string | number ? string | number extends number ? true : unknown : never)
-    ) extends true ? false : true;
-  type phase4 =
-    (
-      (string | number extends string ? true : unknown)
-      |
-      (string | number extends number ? true : unknown)
-    ) extends true ? false : true;
-  type phase5 =
-    (
-      (
-        (string extends string ? true : unknown) |
-        (number extends string ? true : unknown)
-      )
-      |
-      (
-        (string extends number ? true : unknown) |
-        (number extends number ? true : unknown)
-      )
-    ) extends true ? false : true;
-  type phase6 =
-    (
-      (true | unknown)
-      |
-      (unknown | true)
-    ) extends true ? false : true;
-  type phase7 = (true | unknown) extends true ? false : true;
-  type phase8 = true;
+  // type case2 = IsUnion<[string | number]>
+  // type phase1 = IsUnionImpl<[string | number], [string | number]>;
+  // type phase2 =
+  //   (
+  //     [string | number] extends [string | number] ? [string | number] extends [string | number] ? true : unknown : never
+  //   ) extends true ? false : true;
+  // type phase3 =
+  //   (
+  //     [string | number] extends [string | number] ? true : unknown
+  //   ) extends true ? false : true;
+  // type phase4 = (true) extends true ? false : true;
+
+  // type case3 = IsUnion<string | 'a'>
+  // type phase1 = IsUnionImpl<string | 'a', string | 'a'>;
+  // type phase2 =
+  //   (
+  //     string | 'a' extends string | 'a' ? string | 'a' extends string | 'a' ? true : unknown : never
+  //   ) extends true ? false : true;
+  // type phase3 =
+  //   (
+  //     string | 'a' extends string | 'a' ? true : unknown
+  //   ) extends true ? false : true;
+  // type phase4 = (true) extends true ? false : true;
 
 
   type cases = [
