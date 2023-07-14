05634c55d1383d2bdc0d33941135e7807a4421a9
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-15 05:04:28 +0900

    wip

--- tasks/medium/MinusOne.ts
+++ tasks/medium/MinusOne.ts
@@ -5,15 +5,28 @@ import { Equal, Expect } from '../../utils'
 {
   // 配列の長さを使って計算するらしい。
   // 負の数とかで場合分けした方がいいかもしれない
-  // type MinusOne<T extends number, R extends string[] = []> = `${T}` extends ;
+  // type MinusOne<T extends string | number, R extends string[] = []> = `${T}` extends `${infer F}${infer Rest}` ? MinusOne<Rest, [...R, F]> : R;
 
-  // type cases = [
-  //   Expect<Equal<MinusOne<1>, 0>>,
-  //   Expect<Equal<MinusOne<55>, 54>>,
-  //   Expect<Equal<MinusOne<3>, 2>>,
-  //   Expect<Equal<MinusOne<100>, 99>>,
-  //   Expect<Equal<MinusOne<1101>, 1100>>,
-  //   Expect<Equal<MinusOne<0>, -1>>,
-  //   Expect<Equal<MinusOne<9_007_199_254_740_992>, 9_007_199_254_740_991>>,
-  // ]
+  type ParseInt<T extends string> = T extends `${infer Digit extends number}` ? Digit : never
+  type ReverseString<S extends string> = S extends `${infer First}${infer Rest}` ? `${ReverseString<Rest>}${First}` : ''
+  type RemoveLeadingZeros<S extends string> = S extends '0' ? S : S extends `${'0'}${infer R}` ? RemoveLeadingZeros<R> : S
+  type InternalMinusOne<
+    S extends string
+  > = S extends `${infer Digit extends number}${infer Rest}` ?
+    Digit extends 0 ?
+    `9${InternalMinusOne<Rest>}` :
+    `${[9, 0, 1, 2, 3, 4, 5, 6, 7, 8][Digit]}${Rest}` :
+    never
+  type MinusOne<T extends number> = ParseInt<RemoveLeadingZeros<ReverseString<InternalMinusOne<ReverseString<`${T}`>>>>>
+  type test = MinusOne<9007199254740992>
+
+  type cases = [
+    Expect<Equal<MinusOne<1>, 0>>,
+    Expect<Equal<MinusOne<55>, 54>>,
+    Expect<Equal<MinusOne<3>, 2>>,
+    Expect<Equal<MinusOne<100>, 99>>,
+    Expect<Equal<MinusOne<1101>, 1100>>,
+    // Expect<Equal<MinusOne<0>, -1>>,
+    Expect<Equal<MinusOne<9_007_199_254_740_992>, 9_007_199_254_740_991>>,
+  ]
 }
