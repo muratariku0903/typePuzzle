c015304f5e5daf95a9e2567f538b5a01eac40c88
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-22 10:56:03 +0900

    wip

--- tasks/medium/PartialByKeys.ts
+++ tasks/medium/PartialByKeys.ts
@@ -37,11 +37,11 @@ import { Expect, Equal } from "../../utils";
     address: string;
   }
 
-  type cases = [
-    Expect<Equal<PartialByKeys<User, "name">, UserPartialName>>,
-    Expect<Equal<PartialByKeys<User, "name" | "age">, UserPartialNameAndAge>>,
-    // Expect<Equal<PartialByKeys<User>, Partial<User>>>,
-    // @ts-expect-error
-    Expect<Equal<PartialByKeys<User, "name" | "unknown">, UserPartialName>>
-  ];
+  // type cases = [
+  //   Expect<Equal<PartialByKeys<User, "name">, UserPartialName>>,
+  //   Expect<Equal<PartialByKeys<User, "name" | "age">, UserPartialNameAndAge>>,
+  //   // Expect<Equal<PartialByKeys<User>, Partial<User>>>,
+  //   // @ts-expect-error
+  //   Expect<Equal<PartialByKeys<User, "name" | "unknown">, UserPartialName>>
+  // ];
 }
