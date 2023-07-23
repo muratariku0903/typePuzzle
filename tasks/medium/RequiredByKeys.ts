09d390bb32f3866282636920cb191d3a5ac84e37
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-23 17:05:01 +0900

    wip

---
+++ tasks/medium/RequiredByKeys.ts
@@ -0,0 +1,49 @@
+// Implement a generic RequiredByKeys<T,  K> which takes two type argument T and K.
+
+// K specify the set of properties of T that should set to be required. When K is not provided, it should make all properties required just like the normal Required<T>.
+
+import { Equal, Expect } from "../../utils";
+
+{
+  type IntersectionObj<O extends object> = {
+    [key in keyof O]: Exclude<O[key], undefined>;
+  };
+
+  type RequiredByKeys<O extends object, K extends string | never = never> = [
+    K
+  ] extends [never]
+    ? Required<O>
+    : IntersectionObj<
+        {
+          [key in keyof O as key extends K ? never : key]: O[key];
+        } & {
+          [key in Extract<keyof O, K>]-?: O[key];
+        }
+      >;
+
+  type test = RequiredByKeys<User, "name" | "age">;
+
+  interface User {
+    name?: string;
+    age?: number;
+    address?: string;
+  }
+
+  interface UserRequiredName {
+    name: string;
+    age?: number;
+    address?: string;
+  }
+
+  interface UserRequiredNameAndAge {
+    name: string;
+    age: number;
+    address?: string;
+  }
+
+  type cases = [
+    Expect<Equal<RequiredByKeys<User, "name">, UserRequiredName>>,
+    Expect<Equal<RequiredByKeys<User, "name" | "age">, UserRequiredNameAndAge>>,
+    Expect<Equal<RequiredByKeys<User>, Required<User>>>
+  ];
+}
