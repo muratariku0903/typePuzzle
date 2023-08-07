368091fd4195d8b353a36a41ffc8ffc46e93219f
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-08-07 23:36:23 +0900

    wip

---
+++ tasks/medium/Shift.ts
@@ -0,0 +1,16 @@
+import { Expect, Equal } from "../../utils";
+
+{
+  type Shift<T extends any[]> = T extends [infer F, ...infer Rest] ? Rest : [];
+
+  type test = Shift<[3, 2, 1]>;
+
+  type cases = [
+    // @ts-expect-error
+    Shift<unknown>,
+    Expect<Equal<Shift<[]>, []>>,
+    Expect<Equal<Shift<[1]>, []>>,
+    Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+    Expect<Equal<Shift<["a", "b", "c", "d"]>, ["b", "c", "d"]>>
+  ];
+}
