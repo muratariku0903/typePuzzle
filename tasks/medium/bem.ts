66f5fee0882f005d166d6c6baef050a31e0bed17
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-08-23 19:17:56 +0900

    wip

---
+++ tasks/medium/bem.ts
@@ -0,0 +1,38 @@
+// The Block, Element, Modifier methodology (BEM) is a popular naming convention for classes in CSS.
+
+// For example, the block component would be represented as btn, element that depends upon the block would be represented as btn__price, modifier that changes the style of the block would be represented as btn--big or btn__price--warning.
+
+// Implement BEM<B, E, M> which generate string union from these three parameters. Where B is a string literal, E and M are string arrays (can be empty).
+
+import { Expect, Equal } from "../../utils";
+
+{
+  type BEM<B, E extends any[], M extends any[]> = E extends [
+    infer EF,
+    ...infer ER
+  ]
+    ? M extends [infer MF, ...infer MR]
+      ? null
+      : BEM<`${B}__${EF}`, ER, M>
+    : M extends [infer MF, ...infer MR]
+    ? BEM<`${B}--${}`, ER, M>
+    : B;
+
+  type test = BEM<"btn", ["price"], []>;
+
+  type cases = [
+    Expect<Equal<BEM<"btn", ["price"], []>, "btn__price">>,
+    Expect<
+      Equal<
+        BEM<"btn", ["price"], ["warning", "success"]>,
+        "btn__price--warning" | "btn__price--success"
+      >
+    >,
+    Expect<
+      Equal<
+        BEM<"btn", [], ["small", "medium", "large"]>,
+        "btn--small" | "btn--medium" | "btn--large"
+      >
+    >
+  ];
+}
